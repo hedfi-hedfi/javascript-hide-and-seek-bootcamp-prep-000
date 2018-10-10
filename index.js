e985fa20de2800b37e07e23072ca45fa52c499cc
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-10 16:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget()
+function nestedTarget() {
+  return querySelectorAll('#nested.target');
+}
